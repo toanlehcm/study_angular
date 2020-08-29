de483ad274acb3b0b1a366508003f93f09a872f5
Toan Le <toanlt@imt-soft.com>
2020-08-29 18:34:21 +0700

    lesson 5 component

--- demoAngular/src/app/app.component.ts
+++ demoAngular/src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root', //document.querySelector('app-root'); <app-root></app-root>
-  templateUrl: './app.component.html',
+  // templateUrl: './app.component.html',
+  templateUrl: `
+    <h1>welcome</h1>
+    <app-hello></app-hello>
+    <app-hi></app-hi>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
---
+++ demoAngular/src/app/components/hello/hello.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: `
+    <h2>hello component</h2>
+  `
+})
+export class HelloComponent {}
