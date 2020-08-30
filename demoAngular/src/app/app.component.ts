b78d1c26f7284a2fd080367f5ab4910dd3b44a7f
Toan Le <toanlt@imt-soft.com>
2020-08-30 16:57:34 +0700

    L5 update component

--- demoAngular/src/app/app.component.ts
+++ demoAngular/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root', //document.querySelector('app-root'); <app-root></app-root>
   // templateUrl: './app.component.html',
-  templateUrl: `
-    <h1>welcome</h1>
+  template: `
+    <h1>welcome app</h1>
     <app-hello></app-hello>
     <app-hi></app-hi>
   `,
--- demoAngular/src/app/components/hello/hello.component.ts
+++ demoAngular/src/app/components/hello/hello.component.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-hello',
-  templateUrl: `
-    <h2>hello component</h2>
-  `
+  // templateUrl: './hello.component.html',
+  template: `<h1>Hello world</h1>`,
 })
 export class HelloComponent {}
